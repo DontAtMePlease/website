d1f544d5d4bd38ffcbc3243dd4e6ac2e778a6ca4
Dont@MePlease <57662320+DontAtMePlease@users.noreply.github.com>
2020-01-09 17:24:16 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function openPaste(script) {
+  if (script == "EngName") {
+    window.open("https://anonfile.com/deGaG5Lbn3/Gang_Unit_V3RM_rar", "_blank");
+  } else if (script == "v3rm") {
+    window.open(
+      "https://v3rmillion.net/member.php?action=profile&uid=1444976",
+      "_blank"
+    );
+  } else if (script == "Discord") {
+    window.open("https://profiles.pw/profile/249491761590829057", "_blank");
+  }
+}
